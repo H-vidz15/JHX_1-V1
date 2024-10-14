86cb983af9c131e02e3f90116bc1293dab44c8db
H-vidz15 <hiltonvidz15@gmail.com>
2024-10-14 18:12:48 +0100

    Update _updater.js

--- plugins/_updater.js
+++ plugins/_updater.js
@@ -46,7 +46,7 @@ try {
         let commits = await DB.syncgit();
         if (commits.total === 0)
           return await citel.reply(
-            `*QUEEN_ANITA-V2 IS RUNNING ON LATEST\nPATCHES\nFIXES\UPGRADES*`
+            `*JHX_1-V1 IS RUNNING ON LATEST\nPATCHES\nFIXES\UPGRADES*`
           );
         let update = await DB.sync();
         await citel.bot.sendMessage(
